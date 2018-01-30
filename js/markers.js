82e4f0c3ce81d82c0bf06a0cad09692bc12392e6
Adrian Carabetta <adrian_carabetta@hotmail.com>
2018-01-30 13:17:29 +0800

    Added the rest of the markers I want and wrapped the map and side column ina container with flex

--- js/markers.js
+++ js/markers.js
@@ -30,5 +30,9 @@ var locations = [
         type: 'Entertainment'
     },*/
     {title: 'Run Amuk', location: {lat: -32.0722797, lng: 115.7531305}},
+    {title: 'Common Ground', location: {lat: -32.0546596, lng: 115.7454984}},
+    {title: 'Bread in Common', location: {lat: -32.0562259, lng: 115.745232}},
+    {title: 'Creatures Nextdoor', location: {lat: -32.0588735, lng: 115.743792}},
+    {title: 'Young George', location: {lat: -32.043963, lng: 115.76014}},
     //{title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
