b6f8e99d58eb19415cfe0cf78c6b63b78b2dcd33
Adrian Carabetta <adrian_carabetta@hotmail.com>
2018-06-16 08:00:56 +0800

    added id to associative array (still not working)

--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,11 @@
 var locations = [
-    {title: 'Run Amuk', location: {lat: -32.0722797, lng: 115.7531305}, show: true},
-    {title: 'Common Ground', location: {lat: -32.0546596, lng: 115.7454984}, show: true},
-    {title: 'Bread in Common', location: {lat: -32.0562259, lng: 115.745232}, show: true, show: true, show: true},
-    {title: 'Creatures Nextdoor', location: {lat: -32.0588735, lng: 115.743792}, show: true, show: true},
-    {title: 'Young George', location: {lat: -32.043963, lng: 115.76014}, show: true},
-    //{title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    {title: 'Run Amuk', location: {lat: -32.0722797, lng: 115.7531305}, show: true, zomid: '16601583'},
+    {title: 'Common Ground', location: {lat: -32.0546596, lng: 115.7454984}, show: true, zomid: '16601583'},
+    {title: 'Bread in Common', location: {lat: -32.0562259, lng: 115.745232}, show: true, zomid: '16601583'},
+    {title: 'Creatures Nextdoor', location: {lat: -32.0588735, lng: 115.743792}, show: true, zomid: '16601583'},
+    {title: 'Young George', location: {lat: -32.043963, lng: 115.76014}, show: true, zomid: '16601583'}
 ];
-
+console.log(locations[2].zomid);
 //global map variable
 var map;
 
@@ -16,26 +15,28 @@ var map;
 var viewModel = function() {
   var self = this;
 
-  // Create a new blank array for all the listing markers.
+
   self.errorDisplay = ko.observable('');
+  // Create a new blank array for all the listing markers.
   self.placeMarkers = [];
 
   var largeInfoWindow = new google.maps.InfoWindow();
   // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
-    //var title = locations[i].title;
     var position = locations[i].location;
-
+    var zomatoId = locations[i].zomid;
+    console.log(zomatoId);
     var marker = new google.maps.Marker({
       map: map,
       position: position,
       title: locations[i].title,
-      id: i,
+      zomato: locations[i].zomid,
       show: ko.observable(locations[i].show),
-      animation: google.maps.Animation.DROP
+      animation: google.maps.Animation.DROP,
+      // info: info
     });
-
     self.placeMarkers.push(marker);
+    console.log(marker.zomato);
     // Create an onclick event to open an infowindow at each marker
     marker.addListener('click', function(){
       self.populateInfoWindow(this, largeInfoWindow);
@@ -50,7 +51,41 @@ var viewModel = function() {
     // Check to make sure the infowindow is not already open on this marker
     if(infowindow.marker != marker) {
       infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
+
+
+
+      // infowindow.setContent('<div>' + marker.title + '</div>' + '<iframe href="https://developers.zomato.com/api/v2.1/reviews?res_id=16601583"></iframe>');
+      // infowindow.setContent('<div>' + marker.title + '</div>' + '<iframe href="' + self.marker.info + '"></iframe>');
+
+      $.ajax({
+        url: "https://developers.zomato.com/api/v2.1/reviews?res_id=" + marker.zomato,
+        dataType: 'json',
+        async: true,
+        beforeSend: function(xhr){xhr.setRequestHeader('user-key', 'b89e6ac3af9aa9810070ab995f44111c');},  // This inserts the api key into the HTTP header
+        success: function( response ) {
+          console.log(response);
+          console.log(response.user_reviews);
+          var reviews = response.user_reviews;
+
+          var contentString = '<div> <h2>' + marker.title + ' reviews</h2>';
+
+          //infowindow.setContent('<iframe href="' + $.ajax(). + '"></iframe>');
+          //infowindow.append('<h4>Username: ' + reviews[i].review.user.name + '</h4>');
+           for (var i = 0; i < reviews.length; i++){
+             contentString = contentString + '<div>';
+              contentString = contentString + '<h4>User: ' + reviews[i].review.user.name + '</h4>';
+              contentString = contentString + '<p>Rating: ' + reviews[i].review.rating + '</p>';
+              contentString = contentString + '<p>Review: ' + reviews[i].review.review_text + '</p>';
+            contentString = contentString + '<div>';
+             console.log(reviews[i].review.user);
+
+           };
+           contentString = contentString + '</div>';
+          infowindow.setContent(contentString);
+        }
+      });
+
+      //infowindow.setContent('<iframe href="' + $.ajax(). + '"></iframe>');
       infowindow.open(map, marker);
       // Make sure the marker property is cleared if the infowindow is closed
       infowindow.addListener('closeClick', function(){
@@ -137,3 +172,4 @@ function initMap() {
        });
      ko.applyBindings(new viewModel());
 }
+// b89e6ac3af9aa9810070ab995f44111c
