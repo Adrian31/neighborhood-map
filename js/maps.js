9c909976cb0c0e88a4191e1feda7bb9dfb61396e
Adrian Carabetta <adrian_carabetta@hotmail.com>
2018-07-16 18:53:54 +0800

    working version of ajax request

--- js/maps.js
+++ js/maps.js
@@ -1,11 +1,4 @@
-var locations = [
-    {title: 'Run Amuk', location: {lat: -32.0722797, lng: 115.7531305}, show: true, zomid: '16601583'},
-    {title: 'Common Ground', location: {lat: -32.0546596, lng: 115.7454984}, show: true, zomid: '16601583'},
-    {title: 'Bread in Common', location: {lat: -32.0562259, lng: 115.745232}, show: true, zomid: '16601583'},
-    {title: 'Creatures Nextdoor', location: {lat: -32.0588735, lng: 115.743792}, show: true, zomid: '16601583'},
-    {title: 'Young George', location: {lat: -32.043963, lng: 115.76014}, show: true, zomid: '16601583'}
-];
-console.log(locations[2].zomid);
+
 //global map variable
 var map;
 
@@ -15,75 +8,87 @@ var map;
 var viewModel = function() {
   var self = this;
 
-
   self.errorDisplay = ko.observable('');
   // Create a new blank array for all the listing markers.
   self.placeMarkers = [];
 
   var largeInfoWindow = new google.maps.InfoWindow();
+
   // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
-    var position = locations[i].location;
-    var zomatoId = locations[i].zomid;
-    console.log(zomatoId);
     var marker = new google.maps.Marker({
       map: map,
-      position: position,
       title: locations[i].title,
-      zomato: locations[i].zomid,
+      position: locations[i].location,
       show: ko.observable(locations[i].show),
       animation: google.maps.Animation.DROP,
+      zomatoId: locations[i].zomid
       // info: info
     });
+
     self.placeMarkers.push(marker);
-    console.log(marker.zomato);
+
+
+    marker.addListener('click', function() {
+      for (var i = 0; i < self.placeMarkers.length; i++) {
+        self.placeMarkers[i].setAnimation(null);
+      }
+      toggleBounce(this);
+    });
+
     // Create an onclick event to open an infowindow at each marker
     marker.addListener('click', function(){
       self.populateInfoWindow(this, largeInfoWindow);
-      });
-
+    });
+    // console.log(placeMarkers());
   };
+
+  // Animate marker when selected
+  function toggleBounce(ele) {
+    if (ele.getAnimation() !== null) {
+      ele.setAnimation(null);
+    } else {
+      ele.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
 
   //This function populates the infowindow when the marker is clicked. We'll only allow
   // one infowindow which will open at the marker that is clicked, and populate based
   // on that markers position
+  //
   self.populateInfoWindow = function(marker, infowindow){
     // Check to make sure the infowindow is not already open on this marker
-    if(infowindow.marker != marker) {
-      infowindow.marker = marker;
 
-
-
-      // infowindow.setContent('<div>' + marker.title + '</div>' + '<iframe href="https://developers.zomato.com/api/v2.1/reviews?res_id=16601583"></iframe>');
-      // infowindow.setContent('<div>' + marker.title + '</div>' + '<iframe href="' + self.marker.info + '"></iframe>');
-
+      console.log(marker);
+      // Ajax request from Zomato
       $.ajax({
-        url: "https://developers.zomato.com/api/v2.1/reviews?res_id=" + marker.zomato,
+        url: "https://developers.zomato.com/api/v2.1/reviews?res_id=" + marker.zomatoId,
         dataType: 'json',
-        async: true,
+        // async: true,
         beforeSend: function(xhr){xhr.setRequestHeader('user-key', 'b89e6ac3af9aa9810070ab995f44111c');},  // This inserts the api key into the HTTP header
         success: function( response ) {
-          console.log(response);
-          console.log(response.user_reviews);
           var reviews = response.user_reviews;
+        // console.log(marker.zomatoId);
+          var contentString = '<div> <h3>' + marker.title + ' reviews</h2>';
 
-          var contentString = '<div> <h2>' + marker.title + ' reviews</h2>';
-
-          //infowindow.setContent('<iframe href="' + $.ajax(). + '"></iframe>');
-          //infowindow.append('<h4>Username: ' + reviews[i].review.user.name + '</h4>');
-           for (var i = 0; i < reviews.length; i++){
-             contentString = contentString + '<div>';
-              contentString = contentString + '<h4>User: ' + reviews[i].review.user.name + '</h4>';
+           for (var i = 0; i < 3; i++){
+             contentString = contentString + '<div class="infowindow-data">';
+              contentString = contentString + '<h6>User: ' + reviews[i].review.user.name + '</h4>';
               contentString = contentString + '<p>Rating: ' + reviews[i].review.rating + '</p>';
-              contentString = contentString + '<p>Review: ' + reviews[i].review.review_text + '</p>';
+              // contentString = contentString + '<p>Review: ' + reviews[i].review.review_text + '</p>';
             contentString = contentString + '<div>';
-             console.log(reviews[i].review.user);
-
            };
            contentString = contentString + '</div>';
           infowindow.setContent(contentString);
+        },
+
+        // warn if there is error in recievng json
+        error: function(e) {
+            self.errorDisplay("Hmm! Looks like Zomato isn't available right now.");
+            contentString = "<div><p>Hmm! Looks like Zomato isn't available right now.</p></div>"
         }
       });
+
 
       //infowindow.setContent('<iframe href="' + $.ajax(). + '"></iframe>');
       infowindow.open(map, marker);
@@ -91,8 +96,22 @@ var viewModel = function() {
       infowindow.addListener('closeClick', function(){
         infowindow.setMarker(null);
       });
-    }
+    // }
   }
+
+  //function to add information about API to the markers
+  var addMarkerInfo = function(marker) {
+    infowindow.clear();
+    //add API items to each marker
+    self.populateInfoWindow(marker);
+
+    //add the click event listener to marker
+    marker.addListener('click', function() {
+        //set this marker to the selected state
+
+        self.setMarker(marker);
+    });
+  };
 
   // create a searchText for the input search field
   self.searchText = ko.observable('');
@@ -109,14 +128,13 @@ var viewModel = function() {
 
       //variable for search text
       var currentText = self.searchText();
-      //infowindow.close();
+      // infowindow.close();
 
       //list for user search
       if (currentText.length === 0) {
           self.setAllShow(true);
       } else {
           for (var i = 0; i < self.placeMarkers.length; i++) {
-            console.log(self.placeMarkers[i].show());
               // to check whether the searchText is there in the placeMarkers
               if (self.placeMarkers[i].title.toLowerCase().indexOf(currentText.toLowerCase()) > -1) {
                   self.placeMarkers[i].show(true);
@@ -128,44 +146,25 @@ var viewModel = function() {
               }
           }
       }
-      //infowindow.close();
   };
 
+  // var infoWindowPopulator = function(marker, largeInfoWindow){
+  //   //add API items to each marker
+  //   self.populateInfoWindow(this, largeInfoWindow);
+  // }
+  //
+  // //  iterate through mapArray and add marker api info
+  // for (var i = 0; i < self.placeMarkers.length; i++) {
+  //   infoWindowPopulator(self.placeMarkers[i], largeInfoWindow);
+  // }
+
   self.filterList();
-
-   // // Style the markers a bit. self will be our listing marker icon.
-   // var defaultIcon = makeMarkerIcon('0091ff');
-   // // Create a "highlighted location" marker color for when the user
-   // // mouses over the marker.
-   // var highlightedIcon = makeMarkerIcon('FFFF24');
-   //
-   //   // Two event listeners - one for mouseover, one for mouseout,
-   //   // to change the colors back and forth.
-   //   marker.addListener('mouseover', function() {
-   //     self.setIcon(highlightedIcon);
-   //   });
-   //   marker.addListener('mouseout', function() {
-   //     self.setIcon(defaultIcon);
-   //   });
-
-
-   function makeMarkerIcon(markerColor) {
-     var markerImage = new google.maps.MarkerImage(
-       'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-       '|40|_|%E2%80%A2',
-       new google.maps.Size(21, 34),
-       new google.maps.Point(0, 0),
-       new google.maps.Point(10, 34),
-       new google.maps.Size(21,34));
-     return markerImage;
-   }
 };
 
 function initMap() {
-
        // Constructor creates a new map - only center and zoom are required.
        map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -32.0501672, lng: 115.7587907},
+         center: {lat: -32.04199, lng: 115.7507386},
          zoom: 13,
          styles: styles,
          mapTypeControl: false
--- js/markers.js
+++ js/markers.js
@@ -1,38 +1,7 @@
 var locations = [
-    /*{
-        title: 'Run Amuk',
-        lat: -32.0722751,
-        lng:  -115.7509418,
-        type: 'Snack Bar'
-    },
-    {
-        title: 'Nandos Restaurant',
-        lat: 51.5023146,
-        lng: -0.0187593,
-        type: 'Restaurant'
-    },
-    {
-        title: 'Canary Wharf Tube Station',
-        lat: 51.5034898,
-        lng: -0.0185944,
-        type:  'Transit'
-    },
-    {
-        title: 'One Canada Square',
-        lat: 51.5049494,
-        lng: -0.0194997,
-        type:  'Shopping'
-    },
-    {
-        title: 'The O2',
-        lat: 51.503039,
-        lng: 0.003154,
-        type: 'Entertainment'
-    },*/
-    {title: 'Run Amuk', location: {lat: -32.0722797, lng: 115.7531305}},
-    {title: 'Common Ground', location: {lat: -32.0546596, lng: 115.7454984}},
-    {title: 'Bread in Common', location: {lat: -32.0562259, lng: 115.745232}},
-    {title: 'Creatures Nextdoor', location: {lat: -32.0588735, lng: 115.743792}},
-    {title: 'Young George', location: {lat: -32.043963, lng: 115.76014}},
-    //{title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    {title: 'Run Amuk', location: {lat: -32.0722797, lng: 115.7531305}, show: true, zomid: '16598837'},
+    {title: 'Cafe Lumos', location: {lat: -32.0590156, lng: 115.7461505}, show: true, zomid: '16597510'},
+    {title: 'Bread in Common', location: {lat: -32.0562259, lng: 115.745232}, show: true, zomid: '16600553'},
+    {title: 'Creatures Nextdoor', location: {lat: -32.0588735, lng: 115.743792}, show: true, zomid: '16595920'},
+    {title: 'Young George', location: {lat: -32.043963, lng: 115.76014}, show: true, zomid: '16601583'}
 ];
